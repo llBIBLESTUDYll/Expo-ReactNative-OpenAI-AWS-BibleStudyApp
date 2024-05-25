ba3f111619c2756187bf82987ea5ef16243473e1
Ali <ali.lee.0923@gmail.com>
2024-05-25 09:58:38 -0400

    modify home style

--- app/screens/SessionScreen.js
+++ app/screens/SessionScreen.js
@@ -1,71 +1,46 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
+import { styled, withExpoSnack } from 'nativewind';
 import { useNavigation } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
+import { styles } from '../styles/SessionStyle'
+
+const StyledText = styled(Text);
 
 const user = "Rueben";
 
 const SessionScreen = ({ navigation }) => {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcomeMessage}>Welcome back,</Text>
-        <Text style={styles.welcomeMessage}>{user}!</Text>
-        <View style={styles.verseContainer}>
-          <Text style={styles.verseTitle}>Weekly Verse</Text>
-          <Text style={styles.verseText}>John 3:16 - "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life."</Text>
-        </View>
-        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('CreateSession')}>
-          <Text style={styles.buttonText}>CREATE NEW STUDY</Text>
-          {/* <Ionicons name="caret-forward" size={24} color="black" /> */}
+  return (
+    <View className={styles.container}>
+      <View></View>
+      <View>
+        <StyledText className={styles.welcomeText}>Welcome back,</StyledText>
+        <Text className={styles.welcomeText}>{user}!</Text>
+      </View>
+      <View className={styles.homeLabel.background}>
+        <Text className={styles.homeLabel.title}>Weekly Verse</Text>
+        <Text className={styles.homeLabel.content}>John 3:16 - "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life."</Text>
+      </View>
+        <TouchableOpacity className={styles.button} onPress={() => navigation.navigate('CreateSession')}>
+          <Text className={styles.buttonText}>
+            CREATE NEW STUDY
+          </Text>
+          <Ionicons className={styles.caret} name="caret-forward" size={28} color="black" />
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('CreateSession')}>
-          <Text style={styles.buttonText}>MY STUDIES</Text>
+        <TouchableOpacity className={styles.button} onPress={() => navigation.navigate('CreateSession')}>
+          <Text className={styles.buttonText}>
+            MY STUDIES
+          </Text>
+          <Ionicons className={styles.caret} name="caret-forward" size={28} color="black" />
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('CreateSession')}>
-          <Text style={styles.buttonText}>COMMUNITY STUDIES</Text>
+        <TouchableOpacity className={styles.button} onPress={() => navigation.navigate('CreateSession')}>
+          <Text className={styles.buttonText}>
+            COMMUNITY STUDIES
+          </Text>
+          <Ionicons className={styles.caret} name="caret-forward" size={28} color="black" />
         </TouchableOpacity>
-    </View>
+  </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    padding: 20,
-    paddingTop: 40,
-  },
-  welcomeMessage: {
-    fontSize: 24,
-    textAlign: 'left',
-    margin: 5,
-  },
-  verseContainer: {
-    backgroundColor: '#E8E8E8',
-    padding: 20,
-    borderRadius: 10,
-    marginTop: 30,
-    marginBottom: 30,
-  },
-  verseTitle: {
-    fontSize: 20,
-    marginBottom: 10,
-  },
-  verseText: {
-    fontSize: 16,
-    fontStyle: 'italic',
-  },
-  button: {
-    backgroundColor: '#444444',
-    padding: 30,
-    borderRadius: 10,
-    marginBottom: 10,
-  },
-  buttonText: {
-    color: '#fff',
-    textAlign: 'center',
-    fontSize: 16,
-  },
-});
-
-export default SessionScreen;
+export default withExpoSnack(SessionScreen);
---
+++ app/styles/SessionStyle.js
@@ -0,0 +1,12 @@
+export const styles = {
+  container: 'p-8',
+  welcomeText: 'text-3xl',
+  homeLabel: {
+    background: 'bg-gray-300 p-4 rounded-3xl my-5',
+    title: 'text-lg mb-2 font-bold',
+    content: 'text-md italic'
+  },
+  button: 'p-6 rounded-xl mb-4 border-gray-300 border-r-2 border-b-2 flex flex-row  justify-center',
+  buttonText: 'text-xl text-center',
+  caret: 'right-1'
+};
